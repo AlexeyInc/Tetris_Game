3b183339bbd0d3a89450b7e097cd5ecc32b902a4
AlekseiStalker <allah420@protonmail.com>
2017-12-16 10:36:56 +0200

    Revert "Revert "podvel_itogi""
    This reverts commit 471ff012ac27a100b1988016e1dfbcf337def934.

---
+++ unity_tetris/Assets/Scripts/View.cs
@@ -0,0 +1,27 @@
+﻿ using UnityEngine;
+using System.Collections.Generic;
+using MyFSM;
+
+public class View : MonoBehaviour {
+
+    private static View _instance;
+
+    public GameObject[] UI_Elements; 
+
+    public static View Instance {
+        get {
+            if (_instance == null) {
+                return new View();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake() {
+        _instance = this; 
+    }  
+
+    public void OnMenuButtonClick_Name(string newStateName) {
+        Controller.Instance.ChangeScenario(newStateName);
+    } 
+}
---
+++ vs_fsm_test/MyFSM_ConsoleTest/Controller.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyFSM;
+
+namespace MyFSM_ConsoleTest {
+    class Controller {
+        static Controller _instance;
+
+        StateMachine _stateMachine = null; 
+
+        public static Controller Instance {
+            get {
+                if (_instance == null) {
+                    return new Controller();
+                }
+                return _instance;
+            }
+        }
+
+        public Controller() {
+            _instance = this;
+            _stateMachine = new StateMachine(); 
+
+            InitUI();
+        }
+
+        private void InitUI() {
+
+            State[] arrStates = new State[5] { new State ("MainMenu", 0),
+                                           new State ("Game", 1),
+                                           new State ("Help", 2),
+                                           new State ("GameRules", 3),
+                                           new State ("About", 4) };
+            foreach (State st in arrStates) {
+                st.OnStateEnter += OnUIActionEnter;
+                st.OnStateExit += OnUIActionExit;
+            } 
+
+            _stateMachine.IsFsmActive = true;
+        }
+
+        public void ChangeScenario(int numState) {
+            if (numState == 5 || numState == 6) { 
+                Program.listConsoleMenu[numState]();
+                return;
+            } 
+
+            _stateMachine.SwitchState(numState);
+        }
+
+        void OnUIActionEnter() {
+            if (_stateMachine == null || _stateMachine.CurActiveState == null) return;
+
+            int val = _stateMachine.CurActiveState.keyValue;
+            if ((val == -1) || Program.listConsoleMenu.Count == 0) return;
+            if (val >= Program.listConsoleMenu.Count) return;
+
+            Program.listConsoleMenu[val]();
+        }
+
+        void OnUIActionExit() {
+            if (_stateMachine == null || _stateMachine.CurActiveState == null) return; 
+
+            Program.OffCurrentState(_stateMachine.CurActiveState.Name);
+        }
+    }
+}
